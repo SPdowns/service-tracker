64f9c1c3f11861935b3c67d7416fbf9d0fd85203
Sean Downs <seandowns@Seans-MacBook-Pro.local>
2020-09-18 15:11:59 -0700

    starts to build out the soda form. tries to do some header styling

--- src/components/NewSodaForm.js
+++ src/components/NewSodaForm.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { v4 } from "uuid";
 
-function NewSodaForm(props){
+function NewSodaForm(){
+  function handleNewSodaFormSubmission(event) {
+    event.preventDefault();
+  }
   return (
     <React.Fragment>
-      
+      <form onSubmit={handleNewSodaFormSubmission}>
+        <input
+          type='text'
+          name='names'
+          placeholder='Soda Names' />
+        <input
+          type='text'
+          name='price'
+          placeholder='Price' />
+        <textarea
+          name='slogan'
+          placeholder="Type the soda's slogan." />
+        <button type='submit'>Help!</button>
+      </form>
     </React.Fragment>
   );
 }
