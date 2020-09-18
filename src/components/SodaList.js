48969859d96613772db34748ce79a2cb1f1f358e
Sean Downs <seandowns@Seans-MacBook-Pro.local>
2020-09-18 12:11:05 -0700

    build out SodaList

--- src/components/SodaList.js
+++ src/components/SodaList.js
@@ -4,7 +4,15 @@ import Soda from "./Soda"
 function SodaList(){
   return (
     <React.Fragment>
-      
+      <hr/>
+      {masterSodaList.map((soda, index) =>
+      <Soda names={soda.names}
+        location={soda.price}
+        issue={soda.slogan}
+        key={index}/>
+    )}
     </React.Fragment>
-  )
+  );
 }
+
+export default SodaList
