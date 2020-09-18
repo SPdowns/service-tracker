2214d6c9b0abed4c5f7a9881c2bc71a5709dbae5
Sean Downs <seandowns@Seans-MacBook-Pro.local>
2020-09-18 12:34:44 -0700

    build out header and starts App.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return (
+      <React.Fragment>
+        <h1>The Pharmacy</h1>
+        <h5>Fill your perscription, sit and enjoy your malted shake, punch up the script with the notes the studio gave you.</h5>
+      </React.Fragment>
+    );
+}
+export default Header;
