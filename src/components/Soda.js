9498e94a41acfe8c1e762b37d6f30b225d2a3a5f
Sean Downs <spdowns@protonmail.com>
2020-09-25 10:23:12 -0700

    starting to build functionality to sell cans of soda

--- src/components/Soda.js
+++ src/components/Soda.js
@@ -7,6 +7,7 @@ function Soda(props){
       <div onClick={()=> props.whenSodaClicked(props.id)}>
         <h3>{props.names} - {props.price}</h3>
         <p><em>{props.slogan}</em></p>
+        <p>{props.remainingCans}</p>
         <hr/>
       </div>
     </React.Fragment>
@@ -17,8 +18,10 @@ Soda.propTypes = {
   names: PropTypes.string,
   price: PropTypes.string,
   slogan: PropTypes.string,
+  remainingCans: PropTypes.number,
   id: PropTypes.string,
-  whenSodaClicked: PropTypes.func
+  whenSodaClicked: PropTypes.func,
+  whenSellCansClicked: PropTypes.func
 };
 
 export default Soda;
--- src/components/SodaDetail.js
+++ src/components/SodaDetail.js
@@ -7,7 +7,8 @@ function SodaDetail(props){
     <React.Fragment>
       <h1>Sodas</h1>
       <h3>{soda.names} - {soda.price}</h3>
-      <p><em>{soda.slogan}</em></p>      
+      <p><em>{soda.slogan}</em></p>
+      <p>{props.remainingCans}</p>
       <button onClick={ props.onClickEdit }>Update Soda</button>
       <button onClick={()=> onClickingDelete(soda.id)}>Delete Soda</button>
       <hr/>
